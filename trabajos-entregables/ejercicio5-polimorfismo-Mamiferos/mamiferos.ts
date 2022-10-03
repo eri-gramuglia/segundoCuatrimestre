d89f40480cb934fe991b23ab654b024bde664c58
eri.gram <ericagramuglia@tdf.edu.ar>
2022-10-02 22:05:59 -0300

    code mamiferos.ts

--- trabajos-entregables/ejercicio5-polimorfismo-Mamiferos/mamiferos.ts
+++ trabajos-entregables/ejercicio5-polimorfismo-Mamiferos/mamiferos.ts
@@ -1,37 +1,44 @@
 export  class Mamifero {
         protected nombre:string;
         protected especie:string;
-        protected desplazamiento:string;
+        protected velocidadDesplazamiento:number;
         protected alimentacion:string;
 
-        constructor (especie:string, desplazamiento:string, alimentacion:string){
+        constructor (nombre:string, especie:string,velocidadDesplazamiento:number, alimentacion:string){
+           this.nombre = nombre;
            this.especie = especie;
-           this.desplazamiento = desplazamiento;
+           this.velocidadDesplazamiento = velocidadDesplazamiento;
            this.alimentacion = alimentacion;
         }
     public getNombre():string{
         return this.nombre;
     }
-    public setNombre(nombre="Lobo"):void{
+    public setNombre(nombre:string):void{
             this.nombre=nombre;
     }
     public getEspecie():string{
         return this.especie;
     }
-    public setEspecie(especie="felino"):void{
+    public setEspecie(especie:string):void{
         this.especie=especie;
     }
 
-    public getDesplazamiento():string{
-        return this.desplazamiento;
+    public getDesplazamiento():number{
+        return this.velocidadDesplazamiento;
     }
-    public setDesplazamiento(desplazamiento="camina"):void{
-        this.desplazamiento=desplazamiento;
+    public setDesplazamiento(velocidadDesplazamiento:number):void{
+        this.velocidadDesplazamiento=velocidadDesplazamiento + 10;
     }
     public getAlimentacion():string{
         return this.alimentacion;
     }
-    public setAlimentacion(alimentacion="hueso"):void{
+    public setAlimentacion(alimentacion:string):void{
         this.alimentacion=alimentacion;
     }
 }
+
+/*let mamiferos1: Mamifero = new Mamifero("Hombre","Humano",10,"carne");
+mamiferos1.setDesplazamiento(10);
+console.log("hola");
+console.log("la velocidad del humano es: "+ mamiferos1.getDesplazamiento());
+*/
